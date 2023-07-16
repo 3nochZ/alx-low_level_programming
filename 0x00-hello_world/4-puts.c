784c509a1721ec1d084dd4ecc471e091af678d2a
Craftsman <henokzjohn@gmail.com>
2023-07-16 09:56:42 -0700

    using_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * puts - prints using puts
  *
  * Description: only using puts, print \"Programming is like
- * 
+ * @\"Programming is like building a multilingual puzzle: text to be printed
  * Return: return 0, if successful. Non-zero on failure.
  */
 int main(void)
