52584064bb69b65679b73484a70e00c556e70fbf
Craftsman <henokzjohn@gmail.com>
2023-07-17 13:06:25 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - lower case
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char letter = 'a';
+	char upper = 'A';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	while (upper <= 'Z');
+	{
+		putchar(upper);
+		upper++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
