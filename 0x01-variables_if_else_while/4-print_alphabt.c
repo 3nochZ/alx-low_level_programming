0001b78a5fe8c232cfcfb06bd8325662533a2862
Craftsman <henokzjohn@gmail.com>
2023-07-17 13:27:58 -0700

    no_q&e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-        char letter = 'a';
+	char letter = 'a';
 
 	while (letter <= 'z')
 	{
 		if (letter != 'q' && letter != 'e')
 		{
 			putchar(letter);
-			letter++;
 		}
+		letter++;
 	}
 
 	putchar('\n');
