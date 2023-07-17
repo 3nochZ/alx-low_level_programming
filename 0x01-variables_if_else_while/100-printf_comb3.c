5c6c702546924b274b61f9bd216eb6e8b20969b1
Craftsman <henokzjohn@gmail.com>
2023-07-17 14:30:36 -0700

    combs2

---
+++ 0x01-variables_if_else_while/100-printf_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 0;
+
+	while (num1 < 9)
+	{
+		int num2 = num1 + 1;
+		
+		while (num2 <= 9)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			if (num1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			num2++;
+		}
+
+		num1++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
