17cfef823b5a361304645430c0e40497f19de264
Craftsman <henokzjohn@gmail.com>
2023-07-17 14:03:29 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - hexadecimal
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	char c = '0';
+	
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'a';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return 0;
+}
