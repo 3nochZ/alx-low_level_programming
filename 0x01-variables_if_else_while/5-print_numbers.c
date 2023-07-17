16ae1dbc75d3f79da9fb77f979425da9349edc29
Craftsman <henokzjohn@gmail.com>
2023-07-17 13:21:35 -0700

    no_q&e

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - lower case
+ * Return: 0 if successful
+ */
+int main(void)
+{
+        char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
