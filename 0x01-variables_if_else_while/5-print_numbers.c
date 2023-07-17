abe0e03e866c0f2b72074db7f14062f90262613b
Craftsman <henokzjohn@gmail.com>
2023-07-17 13:36:57 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - numbers
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
