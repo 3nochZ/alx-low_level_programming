f1ea94a21c36596576bcf79667e94c2f193b84fc
Craftsman <henokzjohn@gmail.com>
2023-07-19 13:48:07 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,33 @@
 void times_table(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			int result = i * j;
+
 			if (result == 0)
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar('0');
+				_putchar(',');
 			}
 			else if (result < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(result + '0');
+				_putchar(',');
 			}
 			else if (result < 100)
 			{
 				_putchar(' ');
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
+				_putchar(',');
 			}
 		}
 		_putchar('\n');
