6301ed313f6b516ef7857fc22978f6c6a5a50cf5
Craftsman <henokzjohn@gmail.com>
2023-07-19 12:20:16 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - detects an alphabet
+ * @c: parameter
+ * Return: 1 if succesful
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}	
