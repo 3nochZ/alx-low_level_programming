0a86eb4feab97df73ad6c05659fa829465c2270f
Craftsman <henokzjohn@gmail.com>
2023-07-19 10:05:03 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -5,20 +5,6 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-
-void print_alphabet(void)
-{       char letter = 'a';
-
-        while (letter <= 'z')
-        {
-                _putchar(letter);
-                letter++;
-        }
-
-        _putchar('\n');
-
-}
 
 int main(void)
 {
