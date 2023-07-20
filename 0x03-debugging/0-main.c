993cc89815b6c1aa54414f3180517509539ed217
Craftsman <henokzjohn@gmail.com>
2023-07-20 08:45:01 -0700

    +-

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test positive or negative with the case of 0
+ * Return: always 0
+ */
+
+int main(void i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postivie_or_negative(int i);
+
+#endif
