c7343990df9c86f5113f2651ad46883c6a76984f
Craftsman <henokzjohn@gmail.com>
2023-07-22 02:54:38 -0700

    exclude_2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 upto 9 without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if ((i == 2) || (i == 4))
+		{
+			i++;
+			continue;
+		}
+		_putchar(i + '0');
+		i++;
+	}
+	
+	_putchar('\n');
+	
+}
