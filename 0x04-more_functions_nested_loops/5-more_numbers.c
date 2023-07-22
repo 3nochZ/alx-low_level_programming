34bf693cd6401f09ab388467997a4036ae26f38b
Craftsman <henokzjohn@gmail.com>
2023-07-22 04:04:19 -0700

    10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14 10 times
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+	int b;
+
+	while (a < 10)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 14)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0'); 
+		}
+		a++;
+		_putchar('\n');
+	}
+}
