c44c5539daf76e84aeb4296052bd000885e0d67d
Craftsman <henokzjohn@gmail.com>
2023-07-23 07:30:21 -0700

    print_dashes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints line using dashes
+ * @n: number of dashes to be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
