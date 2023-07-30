126a721733e1d6feaca3e9f9804927b16e79050c
root <henokzjohn@gmail.com>
2023-07-30 09:30:53 -0700

    reverse2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	char *start = s;
+	char *end = s;
+	char temp;
+
+	while (*end != '\0')
+	{
+		end++;
+		len++;
+	}
+
+	while (start < end)
+	{
+		tmp = *start;
+		*start++ = *end;
+		*end-- = tmp;
+	}
+}
