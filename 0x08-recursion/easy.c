8feea98565b2e6e48f6ba75919a4d949a2a40347
root <henokzjohn@gmail.com>
2023-08-04 00:42:28 -0700

    finally

--- 0x08-recursion/easy.c
+++ 0x08-recursion/easy.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * easy - saves you time
@@ -6,9 +8,16 @@
  * Return: nothing
  */
 
-void easy(char *s)
+int main()
 {
-    char command[100];
+    char s[100];
+    printf("Enter commit message: ");
+    fgets(s, sizeof(s), stdin);
+    s[strcspn(s, "\n")] = '\0';  // Remove trailing newline character
+
+    char command[200];
     sprintf(command, "git add . && git commit -m '%s' && git push", s);
     system(command);
+
+    return 0;
 }
