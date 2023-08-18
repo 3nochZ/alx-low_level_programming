b68637b4155a140f9b3807d3a96940b6edaa2ef9
root <henokzjohn@gmail.com>
2023-08-17 23:47:48 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 	else
 	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-		printf("Age: %.2f\n", d->age >= 0.0 && d->age <= 100.0 ? d->age : -1.0);
+		printf("Age: %.6f\n", d->age >= 0.0 && d->age <= 100.0 ? d->age : -1.0);
 		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
 }
