2bd5c644394cc84af5dd714e592257de92627609
root <henokzjohn@gmail.com>
2023-08-21 00:38:42 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - returns every other character
+ * @str: pointer to string to print
+ */
+
+void puts2(char *str)
+{
+	if (str == NULL)
+	{
+		return;
+	}
+
+	else
+	{
+		int i = 0;
+
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i += 2;
+		}
+	}
+
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,4 +15,6 @@ void rev_string(char *s);
 
 int _strlen(char *s);
 
+void puts2(char *str);
+
 #endif
