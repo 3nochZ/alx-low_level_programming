f8065a31d835fbc30961e5c05a1fbed67fbf5b52
root <henokzjohn@gmail.com>
2023-08-21 04:16:49 -0700

    test

---
+++ printf/0-printf.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++; // Move past '%'
+
+            // Check the conversion specifier
+            switch (*format) {
+                case 'c': {
+                    // Print a single character
+                    int c = va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    // Print a string
+                    const char *s = va_arg(args, const char *);
+                    while (*s != '\0') {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    // Print a literal '%'
+                    putchar('%');
+                    count++;
+                    break;
+                }
+            }
+        } else {
+            // Print regular characters
+            putchar(*format);
+            count++;
+        }
+
+        format++; // Move to the next character in the format string
+    }
+
+    va_end(args);
+    return count;
+}
