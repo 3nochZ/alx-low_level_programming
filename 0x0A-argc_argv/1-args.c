d109f5dcad0ffa2613efbe00693d99b55269e3d4
root <henokzjohn@gmail.com>
2023-09-08 01:04:53 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: array of string literal
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
