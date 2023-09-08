3543370dc050587130143b4e01f6fe4ee8ecfc54
root <henokzjohn@gmail.com>
2023-09-08 05:38:05 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,37 +14,29 @@ int main(int argc, char *argv[])
 	int i, j;
 	int sum = 0;
 
-	if (argc < 3)
+	if (argc < 2)
 	{
-		if (argc == 2)
-		{
-			printf("%s\n", argv[1]);
-			return (1);
-		}
-
-		else
-		{
-			printf("0\n");
-			return (1);
-		}
+		printf("0\n");
+		return (0);
+	
 	}
 
 	else
 	{
 		for (i = 1; i < argc; i++)
 		{
-			for (j = 0; argv[i][j] != 0; j++)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
 				if (!isdigit(argv[i][j]))
 				{
 					printf("Error\n");
 					return (1);
 				}
-
-				sum += atoi(argv[i]);
 			}
+			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
-		return (0);
 	}
+
+	printf("%d\n", sum);
+	return (0);
 }
