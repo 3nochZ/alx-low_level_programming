017300c4b23b33cecd331099179b2e10fde22664
theArtist <henokzjohn@gmail.com>
2023-09-29 10:14:12 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - the function puts but by recursion
+ * @s: pointer to string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(i + 1);
+	}
+
+	_putchar('\n');
+}
