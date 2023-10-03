036607063515c975ce5cf9d3f3fab5092cbb97ca
theArtist <henokzjohn@gmail.com>
2023-10-02 19:23:46 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - gives the command name
+ * @argc: argument count
+ * @argv: array of arguments strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
