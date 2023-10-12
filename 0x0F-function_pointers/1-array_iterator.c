12d33f5292abe3b408401ce2dc3ef977b7247ce7
theArtist <henokzjohn@gmail.com>
2023-10-11 19:47:54 -0700

    fun_ptrs

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers"
+
+/**
+ * array_iterator - iterators over elements of array
+ * @size: number of elements
+ * @array: array
+ * @action: pointer to function
+ */
+
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
