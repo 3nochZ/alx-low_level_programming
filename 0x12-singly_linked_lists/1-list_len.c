96d23ee09cd7b2c04d5486ec5044cc3c9bed7c72
theArtist <henokzjohn@gmail.com>
2023-10-19 19:10:04 -0700

    linked_list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list list_t
+ * @h: ptr to list_h
+ * Return: number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		a++;
+		h = h->next;
+	}
+
+	return (a);
+}
